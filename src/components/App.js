eaf2d5ecc2d30588282e14bff56e98e6a2b7ecee
Liam <liamcavens@googlemail.com>
2020-06-02 17:43:56 +0100

    Added custom hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ResourceList from './ResourceList';
+import UserList from './userList';
 
 const App = () => {
     // Syntax below is array destructuring
@@ -7,6 +8,7 @@ const App = () => {
 
     return (
         <div>
+            <UserList />
             <div>
                 <button onClick={() => setResource('posts')}>Posts</button>
                 <button onClick={() => setResource('todos')}>Todos</button>
